d2dfc27d40421586393a5b4824b426f0f5e24ac4
Carine <carinerandrianarijaona@gmail.com>
2025-03-06 14:13:00 +0100

    Etape 2

--- script.js
+++ script.js
@@ -19,15 +19,34 @@ Rajouter un 2eme joueur à votre jeu.
 Étape 5
 Proposer un mode multi-joueur. Demander à l’utilisateur, combien il y a de joueur et gérer la partie en conséquence.*/
 
-// let matchesAvalaible = 50; // nombre d'allumettes à enlever du reste
+let matchesAvalaible = 50; // nombre d'allumettes à enlever du reste
+const choiceNumberRemove1 = document.querySelector("#matchesRemove");
+const sendButton1 = document.querySelector("#sendNumberMatches");
 
-function removeMatches(matchesAvalaible) {
-    while (matchesAvalaible > 0) {
-        let howManyMatches = parseInt(window.prompt("Combien d'allumettes voulez-vous retirer?"));
-        matchesAvalaible -= howManyMatches;
-        alert(`il reste ${matchesAvalaible}`)
-    }
+function askPlayer() {
+    let howManyMatches = parseInt(window.prompt("Combien d'allumettes voulez-vous retirer?"));
+    return howManyMatches;
+}
+
+function howManyMatchesAvailable(currentMatchesToPrint) {
+    alert(`il reste ${currentMatchesToPrint}`);
+}
+
+function youWin() {
     alert("Tu as gagné");
 }
 
-console.log(removeMatches(matchesAvalaible));
+function removeMatches(currentMatchesAvalaible) {
+    while (currentMatchesAvalaible > 0) {
+        let howManyMatchesRemove = askPlayer(); /* la variable enregistre un résultat alors que la fonction permet de générer un autre résultat*/
+        currentMatchesAvalaible -= howManyMatchesRemove;
+        console.log(currentMatchesAvalaible, howManyMatchesRemove)
+        howManyMatchesAvailable(currentMatchesAvalaible);
+        // howManyMatchesRemove = askPlayer(); /* générer un autre réponse, on réatribue une nouvelle valeur pour qu'elle boucle avec cette nouvelle valeur*/
+    }
+    youWin();
+    return currentMatchesAvalaible;
+}
+
+matchesAvalaible = removeMatches(matchesAvalaible);
+console.log();
